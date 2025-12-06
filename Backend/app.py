0aa14988bc9b9dc1dc8c4c9b1dfce3f02a63e1ab
chaitanya-encegen <chaitanya.encegen@gmail.com>
2025-12-06 17:07:07 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -143,10 +143,20 @@ app = create_app()
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = "https://adoodle-software.onrender.com"
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    origin = request.headers.get("Origin")
+
+    allowed = [
+        "https://adoodle-software.onrender.com",
+        "https://adoodle-backend.onrender.com",
+        "http://localhost:5173"
+    ]
+
+    if origin in allowed:
+        response.headers["Access-Control-Allow-Origin"] = origin
+
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     return response
 
 with app.app_context():
