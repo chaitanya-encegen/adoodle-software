72ddd89273a293a0237aba578a15cd7026861af5
chaitanya-encegen <chaitanya.encegen@gmail.com>
2025-12-06 15:20:15 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -140,6 +140,15 @@ def create_app(db_path=f"sqlite:///{DB_PATH}"):
 # Create App Instance
 # ==========================================================
 app = create_app()
+app = create_app()
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = "https://adoodle-software.onrender.com"
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    return response
 
 
 
